b03a7bab271c8901560e49ea2ceaccad467fc543
Levbert Leger <levbertl@users.noreply.github.com>
2021-03-06 20:38:47 -0500

    code cleanup

--- server.js
+++ server.js
@@ -1,75 +1,89 @@
 'use strict'
-
-// [START app]
 const express = require('express')
+const fetch = require('node-fetch');
+
 const app = express()
-const fs = require('fs')
-const https = require('https')
-const templateUrls = [
-  'https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json',
-  'https://raw.githubusercontent.com/SelfhostedPro/selfhosted_templates/master/Template/template.json',
-]
-let combinedJSON = {
+const portainerDefaultTemplates =  'https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json'
+const templateUrls = [portainerDefaultTemplates]
+const combinedJSON = {
   version: '2',
   templates: [],
 }
+
+function removePortainerDefaultTemplate(){
+  let index = templateUrls.indexOf(portainerDefaultTemplates)
+  if (index === 0){
+    templateUrls.shift()
+  }else if (index > 0){
+    templateUrls.slice(index, 1)
+  }  
+  console.log('removed default')
+}
+
+function appendTemplate(data, url) {
+  const templates = Object.assign(combinedJSON.templates,{})
+  try {
+    data.forEach((te) => {
+      if (!templates.filter((t) => t.title === te.title).length) {
+        templates.push(te)
+      }
+    })
+    combinedJSON.templates = templates
+    console.log('appended: '+ url)
+  } catch (e) {
+    console.log('error appending: '+ url)
+  }  
+}
+
+async function getData(url) {
+  const response = await fetch(url)
+  let data = await response.text()
+  try {
+    data = JSON.parse(data)
+    if (Array.isArray(data)) {
+      appendTemplate(data, url)
+    } else if (
+      Object.keys(data).includes('templates') &&
+      Array.isArray(data.templates)
+    ) {
+      appendTemplate(data.templates, url)
+    }
+  } catch (e) {
+    console.log('Error Appending: ' + url)
+  }
+}
+
+async function getTemplates(){
+  console.log('Number of Urls: ' + templateUrls.length)
+  for (let i = 0; i < templateUrls.length; i++) {
+    await getData(templateUrls[i])
+  }
+}
+
 for (const key in process.env) {
   if (
     Object.hasOwnProperty.call(process.env, key) &&
     key.toLowerCase().startsWith('ptu')
   ) {
     templateUrls.push(process.env[key])
+    console.log('added from env: ' + process.env[key])
+  } else if (
+    Object.hasOwnProperty.call(process.env, key) &&
+    key.toLowerCase() == 'pte_default'
+  ) {
+    if(process.env[key]===false || process.env[key] ==='false'){
+      removePortainerDefaultTemplate()
+    }
   }
 }
-function appendTemplate(data, url) {
-  data.forEach((te) => {
-    if (!combinedJSON.templates.filter((t) => t.title === te.title).length) {
-      combinedJSON.templates.push(te)
-    }
-  })
-  console.log('appended: '+ url)
-}
-function getData(url) {
-  https
-    .get(url, (resp) => {
-      let data = ''
-      resp.on('data', (chunk) => {
-        data += chunk
-      })
-      resp.on('end', () => {
-        try {
-          data = JSON.parse(data)
-          if (Array.isArray(data)) {
-            appendTemplate(data, url)
-          } else if (
-            Object.keys(data).includes('templates') &&
-            Array.isArray(data.templates)
-          ) {
-            appendTemplate(data.templates, url)
-          }
-        } catch (e) {
-          console.log(e)
-        }
-      })
-    })
-    .on('error', (err) => {
-      console.log('Error: ' + err.message)
-    })
-}
 
-templateUrls.forEach((templateUrl) => {
-  getData(templateUrl)
-})
-
-app.get('/templates.json', (req, res) => {
+app.get('/templates.json', async(req, res) => {
+  await getTemplates()
   res.send(combinedJSON)
 })
 
-// Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8020
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
 })
-// [END app]
-
 module.exports = app
